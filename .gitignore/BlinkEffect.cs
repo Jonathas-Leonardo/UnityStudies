be2724b1520c71c93915f1dc48d536307bfaf868
Jonathas <jha.leonardo@gmail.com>
2018-05-11 12:01:51 -0300

    Add files via upload

---
+++ .gitignore/BlinkEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkEffect : MonoBehaviour {
+
+    public bool isVisible;
+
+	// Use this for initialization
+	void Start () {
+        //rend = GetComponent<Renderer>();
+        isVisible = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(Mathf.PingPong(Time.time, 1) > .5f)
+        {
+            isVisible = true;
+        }
+        else
+        {
+            isVisible = false;
+        }
+	}
+}
